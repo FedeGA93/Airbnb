10913c4ee9a545a10abec33ab71fea7d14968f39
fedegomezavalos@gmail.com <fedegomezavalos@gmail.com>
2020-08-29 17:27:55 -0300

    Finished

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css';
+
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p> 2020 no rights reserved this is a Clone Demo for Airbnb</p>
+            <p>Privacy Terms Sitemap Company Details</p> 
+            {/* Transformar esto en links a páginas */}
+        </div>
+    )
+}
+
+export default Footer
